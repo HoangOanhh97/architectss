4265dca80beca2a764df3d8da64841512b8389e3
HoangOanhh97 <hoangoanh9899@gmail.com>
2021-07-28 15:30:00 +0700

    styling

--- s-arch/ClientApp/src/environments/environment.ts
+++ s-arch/ClientApp/src/environments/environment.ts
@@ -1,8 +1,4 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false,
+  production: true,
   api: 'http://localhost:4001/api'
 };
