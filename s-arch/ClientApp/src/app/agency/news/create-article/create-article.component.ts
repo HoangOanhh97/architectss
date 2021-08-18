36aa3234a8fba5e6e48de7d8955572a88c177727
HoangOanhh97 <hoangoanh9899@gmail.com>
2021-08-18 22:20:48 +0700

    FE: styling newa page

--- s-arch/ClientApp/src/app/agency/news/create-article/create-article.component.ts
+++ s-arch/ClientApp/src/app/agency/news/create-article/create-article.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-article.component.scss']
 })
 export class CreateArticleComponent implements OnInit {
-
+  data: any = {};
   constructor() { }
 
   ngOnInit(): void {
